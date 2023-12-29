fbfd29ea8c091740b54e505199c12e0dc353ba0e
knifehand <matthew.j.hurt1@gmail.com>
2023-12-28 17:38:12 -0700

    Initial Commit

---
+++ autoinstall-basic-dev-tools/start-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Ansible
+sudo apt update -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
